80cf7e20508f4c00788daff0fd08468363b47b2c
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2022-04-10 20:11:04 +0100

    pages added for resumejs contacts about and resume

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Wrapper from "../components/Wrapper/index";
+import Projects from "../components/Portfolio/index";
+
+function Portfolio() {
+	return (
+		<Wrapper>
+			<Projects />
+		</Wrapper>
+	);
+}
+export default Portfolio;
---
+++ src/pages/ResumePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resume from "../components/Resume";
+import Wrapper from "../components/Wrapper/index";
+
+function ResumePage() {
+	return (
+		<Wrapper>
+			<Resume />
+		</Wrapper>
+	);
+}
+export default ResumePage;
